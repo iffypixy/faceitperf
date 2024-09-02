25c3c616d47c7c1d3e2a2f9632f7260a5f9510b1
Jimmy Jardland <jimmy@jard.land>
2024-09-02 11:20:45 +0200

    feat: count ADR from when it is available

--- apps/web/src/entities/profile/lib/stats.tsx
+++ apps/web/src/entities/profile/lib/stats.tsx
@@ -44,62 +44,46 @@ export const calculateAverageStats = (matches: GameStats[]) => {
 			weight,
 		};
 
-	const kills = matches
-		.map((match) => +match["Kills"])
-		.reduce((prev, kills) => prev + kills, 0);
+	const matchStats = matches.map((match) => {
+		return {
+			kills: +match["Kills"],
+			deaths: +match["Deaths"],
+			rounds: +match["Rounds"],
+			kpr: +match["K/R Ratio"],
+			adr: +match["ADR"] || +match["K/R Ratio"] * DMG_PER_KILL, // adr was added late june, so estimate it until it was added
+			headshots: +match["Headshots"],
+			assists: +match["Assists"],
+		};
+	});
 
-	const deaths = matches
-		.map((match) => +match["Deaths"])
-		.reduce((prev, deaths) => prev + deaths, 0);
-
+	const kills = matchStats.reduce((prev, stat) => prev + stat.kills, 0);
+	const deaths = matchStats.reduce((prev, stat) => prev + stat.deaths, 0);
 	const kd = kills / deaths || 0;
 
 	const dpr =
-		matches
-			.map((match) => +match["Deaths"] / +match["Rounds"])
-			.reduce((prev, dpr) => prev + dpr, 0) / weight;
-
-	const kpr =
-		matches
-			.map((match) => +match["K/R Ratio"])
-			.reduce((prev, kpr) => prev + kpr, 0) / weight;
-
+		matchStats.reduce((prev, stat) => prev + stat.deaths / stat.rounds, 0) /
+		weight;
+	const kpr = matchStats.reduce((prev, stat) => prev + stat.kpr, 0) / weight;
 	const avgk = kills / weight;
+	const adr = matchStats.reduce((prev, stat) => prev + stat.adr, 0) / weight;
 
-	const adr =
-		matches
-			.map((match) => +match["K/R Ratio"] * DMG_PER_KILL)
-			.reduce((prev, adr) => prev + adr, 0) / weight;
-
-	const hs = matches
-		.map((match) => +match["Headshots"])
-		.reduce((prev, hs) => prev + hs, 0);
-
+	const hs = matchStats.reduce((prev, stat) => prev + stat.headshots, 0);
 	const hsp = (hs / kills) * 100;
-
 	const apr =
-		matches
-			.map((match) => +match["Assists"] / +match["Rounds"])
-			.reduce((prev, apr) => prev + apr, 0) / weight;
+		matchStats.reduce(
+			(prev, stat) => prev + stat.assists / stat.rounds,
+			0,
+		) / weight;
 
 	const kast =
-		matches
-			.map((match) => {
-				const kills = +match["Kills"];
-				const deaths = +match["Deaths"];
-				const assists = +match["Assists"];
-				const rounds = +match["Rounds"];
-				const survived = rounds - deaths;
-				const traded = TRADE_PERCENT * rounds;
-
-				const sum = (kills + assists + survived + traded) * 0.45;
-
-				return Math.min((sum / rounds) * 100, 100);
-			})
-			.reduce((prev, kast) => prev + kast, 0) / weight;
+		matchStats.reduce((prev, stat) => {
+			const survived = stat.rounds - stat.deaths;
+			const traded = TRADE_PERCENT * stat.rounds;
+			const sum = (stat.kills + stat.assists + survived + traded) * 0.45;
+			return prev + Math.min((sum / stat.rounds) * 100, 100);
+		}, 0) / weight;
 
 	const impact = Math.max(2.13 * kpr + 0.42 * apr - 0.41, 0);
-
 	const rating = Math.max(
 		0.0073 * kast +
 			0.3591 * kpr +
