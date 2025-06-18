e44eb76a7389102b6595d06990d6a77f45670c7f
bodkul <bodicetop@gmail.com>
2025-06-18 11:01:53 +0200

    fix: group matches by unique date for correct session display

--- apps/web/src/entities/match/lib/group.tsx
+++ apps/web/src/entities/match/lib/group.tsx
@@ -1,35 +1,19 @@
+import * as datefns from "date-fns";
+
 import {Match} from "../types";
 
 export const groupMatchesIntoSessions = (matches: Match[]) => {
-	const SESSION_INTERVAL_HOURS = 4;
-	const MATCH_DURATION = 0.65;
-
-	const sessions: Match[][] = [];
+	const sessions: Record<string, Match[]> = {};
 
 	matches.forEach((match) => {
-		const time = new Date(match["Created At"]).getTime();
+		const dateOnly = datefns.format(match["Created At"], "dd/MM/yy");
 
-		const empty = sessions.length === 0;
-
-		if (empty) {
-			sessions.push([match]);
-		} else {
-			const lastSession = sessions[sessions.length - 1];
-			const lastMatch = lastSession[lastSession.length - 1];
-
-			const lastMatchTime = new Date(lastMatch["Created At"]).getTime();
-
-			const timeDifference = (lastMatchTime - time) / (1000 * 60 * 60);
-
-			const timeThreshold = SESSION_INTERVAL_HOURS + MATCH_DURATION;
-
-			if (timeDifference <= timeThreshold) {
-				lastSession.push(match);
-			} else {
-				sessions.push([match]);
-			}
+		if (!sessions[dateOnly]) {
+			sessions[dateOnly] = [];
 		}
+
+		sessions[dateOnly].push(match);
 	});
 
-	return sessions;
+	return Object.values(sessions);
 };
