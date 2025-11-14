4c79b48df44ccde219d426ef012f57386b1746db
Ansat <iffypixy@gmail.com>
2025-11-14 20:31:26 +0100

    web: Enable posthog tracking

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,17 +1,26 @@
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
+import posthog from "posthog-js";
+import { PostHogProvider } from "@posthog/react";
 
 import { queryClient } from "@shared/lib/query";
 import { GoogleAnalytics } from "@shared/lib/analytics";
+import { Env } from "@shared/env";
 import { App } from "./app";
 import "./index.css";
 
-const root = document.getElementById("root")!;
+posthog.init(Env.VITE_PUBLIC_POSTHOG_KEY, {
+	api_host: Env.VITE_PUBLIC_POSTHOG_HOST,
+	defaults: "2025-05-24",
+});
 
+const root = document.getElementById("root")!;
 createRoot(root).render(
 	<QueryClientProvider client={queryClient}>
-		<GoogleAnalytics>
-			<App />
-		</GoogleAnalytics>
+		<PostHogProvider client={posthog}>
+			<GoogleAnalytics>
+				<App />
+			</GoogleAnalytics>
+		</PostHogProvider>
 	</QueryClientProvider>,
 );
