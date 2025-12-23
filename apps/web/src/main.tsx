2f220ea5be25b802e6868ef1565f71c39dce21d9
Ansat <iffypixy@gmail.com>
2025-12-23 18:55:07 +0100

    Integrate GoogleAnalytics component in place

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,11 @@
+import { useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
+import ReactGA from "react-ga4";
 import posthog from "posthog-js";
 import { PostHogProvider } from "@posthog/react";
 
 import { queryClient } from "@shared/lib/query";
-import { GoogleAnalytics } from "@shared/lib/analytics";
 import { Env } from "@shared/env";
 import { App } from "./app";
 import "./index.css";
@@ -13,6 +14,15 @@ posthog.init(Env.VITE_PUBLIC_POSTHOG_KEY, {
 	api_host: Env.VITE_PUBLIC_POSTHOG_HOST,
 	defaults: "2025-05-24",
 });
+
+const GoogleAnalytics: React.FC<React.PropsWithChildren> = ({ children }) => {
+	useEffect(() => {
+		ReactGA.initialize(Env.VITE_GA_MEASUREMENT_ID);
+		ReactGA.send({ hitType: "pageview" });
+	}, []);
+
+	return children;
+};
 
 const root = document.getElementById("root")!;
 createRoot(root).render(
