88089f0732f7bb2fa4d1762a6caf8be7667f215a
Ansat <iffypixy@gmail.com>
2025-12-25 22:05:57 +0100

    web: Simplify numbers.ts

--- apps/web/src/shared/lib/numbers.ts
+++ apps/web/src/shared/lib/numbers.ts
@@ -1,12 +1,16 @@
 export function clamp(val: number, min: number = -Infinity, max: number = Infinity): number {
-	return val < min ? min : val > max ? max : val;
+	if (val < min) return min;
+	else if (val > max) return max;
+	return val;
 }
 
 export function toSignedString(n: number): string {
 	if (n === 0) return "0";
-	return (n > 0 ? "+" : "-") + Math.abs(n);
+	const sign = n > 0 ? "+" : "-";
+	return `${sign}${Math.abs(n)}`;
 }
 
-export function divide(a: number, b: number, alt: number = 1) {
-	return a / (b || alt);
+export function divide(a: number, b: number) {
+	if (b === 0) return a;
+	return a / b;
 }
