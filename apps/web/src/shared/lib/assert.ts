d3602005d88337c5eb5ec8bfb0c2e3e928235eea
Ansat <iffypixy@gmail.com>
2025-12-25 22:09:37 +0100

    web: Omit redundant assertNever function

--- apps/web/src/shared/lib/assert.ts
+++ apps/web/src/shared/lib/assert.ts
@@ -1,10 +1,3 @@
-export function assert(
-	condition: unknown,
-	message?: string,
-): asserts condition {
+export function assert(condition: unknown, message?: string): asserts condition {
 	if (!condition) throw new Error(message ?? "assertion failed");
-}
-
-export function assertNever(value: never, message?: string): never {
-	throw new Error(message ?? `unexpected value: ${value}`);
 }
